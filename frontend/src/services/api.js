d1c66e15be52d11c9cd441e90535224b3c1306e5
Nimesh2106 <nimeshsarokar2006@gmail.com>
2025-02-09 15:55:58 +0530

    add deploy backend link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8082/api";
+const API_URL = "https://creditsea-lh3y.onrender.com/api";
 
 export const uploadFile = async (file) => {
     
